07af792cc67500ba87de000b90f55e3e715960ba
Jacob Zirbel <jacobzirbel@gmail.com>
2020-05-02 15:32:00 -0500

    search and display movie info from OMDB

--- index.js
+++ index.js
@@ -8,16 +8,15 @@ const spotify = new Spotify({
 	id: keys.spotify.id,
 	secret: keys.spotify.secret,
 });
-
 const separator = "------------";
+
 function axiosGetEvents(artist) {
-	let url;
-	url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=${keys.bandsInTown.secret}`;
+	let url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=${keys.bandsInTown.secret}`;
 	console.log(url);
-	axios
-		.get(url)
-		.then((response) => response.data.forEach((e) => showEvent(e)))
-		.catch((error) => console.log("error"));
+	axios.get(url).then(eventCallback).catch(errorFn);
+	function eventCallback(response) {
+		(response) => response.data.forEach((e) => showEvent(e));
+	}
 }
 
 function showEvent(event) {
@@ -40,4 +39,17 @@ function showSong(song) {
 		song.name
 	}\nSpotify URL: ${song.external_urls.spotify}\nAlbum: ${song.album.name}`;
 	console.log(result);
+}
+function axiosSearchMovie(query) {
+	let url = `http://www.omdbapi.com/?t=${query}&y=&plot=short&apikey=${keys.ombd.secret}`;
+	axios.get(url).then(movieCallback).catch(errorFn);
+	function movieCallback(response) {
+		console.log(
+			`Title: ${response.data.Title}\nYear: ${response.data.Year}\nIMDB Rating: ${response.data.imdbRating}\nCountry: ${response.data.Country}\nLanguage: ${response.data.Language}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}\n`
+		);
+	}
+}
+axiosSearchMovie("mr nobody");
+function errorFn(err) {
+	console.log("error");
 }
